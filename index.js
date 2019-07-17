93f2ed3ce59db1ffe4f8fbba6c5c77501aade53c
mottox2 <y.mottox2@gmail.com>
2019-07-17 21:21:22 +0900

    Use cloud functions

---
+++ index.js
@@ -0,0 +1,23 @@
+// 4章 はじめてのCloud Functionsまで
+exports.sample = (req, res) => {
+  res.send('Hello World')
+}
+
+// 4章 functions-framework導入以降
+const sample = (req, res) => {
+  res.send('Hello World!')
+}
+
+// 参考: https://github.com/googlesamples/functions-as-a-service
+exports.function = (req, res) => {
+  const paths = {
+    '/sample': sample,
+    '/': () => res.send(Object.keys(paths))
+  }
+  for (const [path, route] of Object.entries(paths)) {
+    if (req.path.startsWith(path)) {
+      return route(req, res)
+    }
+  }
+  res.send('No path found')
+}
