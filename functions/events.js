bb2395eac17e12c9a81d0b25177f85a2b3a96f0c
mottox2 <y.mottox2@gmail.com>
2019-07-22 11:40:12 +0900

    Add event implementation

--- functions/events.js
+++ functions/events.js
@@ -1,14 +1,43 @@
 const axios = require('axios')
 const { WebClient } = require('@slack/web-api')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const body = req.body
-  console.log(body)
+  const event = body.event
 
-  switch (body.type) {
-    case 'url_verification':
-      return res.status(200).send(body.challenge)
+  if (body.type === 'url_verification') {
+    return res.status(200).send(body.challenge)
+  }
+  res.status(200).send()
+  const web = new WebClient(process.env.SLACK_TOKEN)
+
+  switch (event.type) {
+    // https://api.slack.com/events/reaction_added
+    case 'reaction_added':
+      if (event.reaction === 'heart') {
+        // scopeの設定が足りていることを確認すること
+        // https://api.slack.com/methods/channels.history
+        const messages = await web.channels.history({
+          channel: event.item.channel,
+          latest: event.item.ts,
+          inclusive: true,
+          count: 1,
+        })
+        const message = messages.messages[0]
+        web.chat.postMessage({
+          ...message,
+          channel: 'share'
+        })
+      }
+      return
+    // https://api.slack.com/events/channel_created
+    case 'channel_created':
+      web.chat.postMessage({
+        text: `新しいチャンネル「<#${event.channel.id}>」が作成されました。`,
+        channel: '#share'
+      })
+      return
     default:
-      return res.status(200).send()
+      return
   }
 }
